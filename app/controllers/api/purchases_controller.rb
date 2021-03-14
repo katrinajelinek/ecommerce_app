67db9307ad3016a86a7e5eea54b5e26d09ce3b37
katrinajelinek <kmrjelinek@gmail.com>
2021-03-14 13:59:11 -0600

    adds purchases routes, controller actions, and views

--- app/controllers/api/purchases_controller.rb
+++ app/controllers/api/purchases_controller.rb
@@ -1,2 +1,41 @@
 class Api::PurchasesController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @purchase = current_user.purchases
+    render "index.json.jb"
+  end
+
+  def show
+    @purchase = current_user.purchases.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    carted_products = current_user.carted_products.where(checkout: false)
+
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.product.price * carted_product.quantity
+    end
+
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
+    @purchase = Purchase.new(
+      user_id: current_user.id,
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
+    )
+
+    if @purchase.save
+      carted_products.update_all(purchase_id: @purchase.id, checkout: true)
+      render "show.json.jb"
+    else
+      render json: { errors: @purchases.errors.full_messages }
+    end
+  end
+
+  #I decided not to make a destroy action because a user should be able to see past purchases
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    get "/purchases" => "purchases#index"
+    get "/purchases/:id" => "purchases#show"
+    post "/purchases" => "purchases#create"
+
     get "/carted_products" => "carted_products#index"
     post "/carted_products" => "carted_products#create"
     delete "/carted_products/:id" => "carted_products#destroy"
