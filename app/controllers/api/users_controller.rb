1cca97338e585fa4c4c7ad6a02b778df870951a1
katrinajelinek <kmrjelinek@gmail.com>
2021-03-14 09:37:50 -0600

    completes users routes, controller actions, and views

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,52 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render "index.json.jb"
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render "show.json.jb"
+  end
+
   def create
-    user = User.new(
+    @user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
     )
-    if user.save
-      render json: { message: "User created successfully" }, status: :created
+    if @user.save
+      render "show.json.jb"
     else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.first_name = params[:first_name] || @user.first_name
+    @user.last_name = params[:last_name] || @user.last_name
+    @user.email = params[:email] || @user.email
+    if params[:password]
+      if @user.authenticate(params[:old_password])
+        @user.update!(
+          password: params[:password],
+          password_confirmation: params[:password_confirmation],
+        )
+      end
+    end
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: { message: "Your account has been deleted" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    get "/users" => "users#index"
+    get "/users/:id" => "users#show"
     post "/users" => "users#create"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
 
     post "/sessions" => "sessions#create"
   end
