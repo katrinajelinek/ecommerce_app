529686d3b989a213975a8c66519164675383fbfe
katrinajelinek <kmrjelinek@gmail.com>
2021-03-15 21:10:12 -0600

    adds monetize to products model and setting status logic to purchase model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  monetize :price_cents
   has_many :carted_products
   has_many :purchases, through: :carted_products
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,22 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   has_many :carted_products
-  has_many :purchases, through: :carted_products
+  has_many :products, through: :carted_products
+
+  enum status: { pending: 0, failed: 1, paid: 2, paypal_executed: 3 }
+  enum payment_gateway: { stripe: 0, paypal: 1 }
+
+  scope :recently_created, -> { where(created_at: 1.minutes.ago..DateTime.now) }
+
+  def set_paid
+    self.status = Purchase.statuses[:paid]
+  end
+
+  def set_failed
+    self.status = Purchase.statuses[:failed]
+  end
+
+  def set_paypal_executed
+    self.status = Purchase.statuses[:paypal_executed]
+  end
 end
